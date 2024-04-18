aa661dc6d94fadc23b9a3de6cbea3996cdb04373
isabellaperezcav <156531080+isabellaperezcav@users.noreply.github.com>
2024-04-18 10:15:56 -0500

    product microservice

---
+++ microProductos/src/models/productosModel.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'almacen2'
+});
+
+async function traerProductos() {
+    const result = await connection.query('SELECT * FROM productos');
+    return result[0];
+}
+
+async function traerProducto(id) {
+    const result = await connection.query('SELECT * FROM productos WHERE id = ? ', id);
+    return result[0];
+}
+
+async function actualizarProducto(id, inventario) {
+    const result = await connection.query('UPDATE productos SET inventario = ? WHERE id = ? ', [inventario, id]);
+    return result;
+}
+
+async function crearProducto(nombre, precio, inventario) {
+    const result = await connection.query('INSERT INTO productos VALUES(null,?,?,?)', [nombre, precio, inventario]);
+    return result;
+}
+
+module.exports = {
+    traerProductos, traerProducto, actualizarProducto, crearProducto
+};
