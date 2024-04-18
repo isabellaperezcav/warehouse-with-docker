e9b1a769ec5dfcd1d4e7ecfd0defb3ca88809c8c
isabellaperezcav <156531080+isabellaperezcav@users.noreply.github.com>
2024-04-18 10:18:22 -0500

    user microservice

---
+++ microUsuarios/src/models/usuariosModel.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql2/promise');
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'almacen2'
+});
+async function traerUsuarios() {
+    const result = await connection.query('SELECT * FROM usuarios');
+    return result[0];
+}
+async function traerUsuario(usuario) {
+    const result = await connection.query('SELECT * FROM usuarios WHERE usuario = ? ', usuario);
+    return result[0];
+}
+
+async function validarUsuario(usuario, password) {
+    const result = await connection.query('SELECT * FROM usuarios WHERE usuario = ? AND password = ?', [usuario, password]);
+    return result[0];
+}
+
+async function crearUsuario(nombre, email, usuario, password) {
+    const result = await connection.query('INSERT INTO usuarios VALUES(?,?,?,?)', [nombre, email, usuario, password]);
+    return result;
+}
+
+module.exports = {
+    traerUsuarios, traerUsuario, validarUsuario, crearUsuario
+};
