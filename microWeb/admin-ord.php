22834dd3b15e626c27b0cddf0155a954a7757b6f
isabellaperezcav <156531080+isabellaperezcav@users.noreply.github.com>
2024-04-23 21:27:13 -0500

    microWeb

--- microWeb/admin-ord.php
+++ microWeb/admin-ord.php
@@ -5,7 +5,6 @@
             header("Location: index.html");
         }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,8 +15,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
-<body>
-    
+<body>  
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="admin.php">Almacen ABC</a>
